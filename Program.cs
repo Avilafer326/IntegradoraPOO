a88e6ffbf0e8994c3acb36caca2e3edea0f8501e
Fernando Ávila <Avilafer614@gmail.com>
2025-10-16 08:40:35 -0600

    Mensaje en Program

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace IntegradoraPOO
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //cambio que se hizo desde local
         }
     }
 }
