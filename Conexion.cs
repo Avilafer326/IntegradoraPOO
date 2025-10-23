c005dee0c86a5264fc0c17dc500885fb515d29e5
Fernando Ávila <Avilafer614@gmail.com>
2025-10-23 14:38:07 -0600

    ps abr q puse pero ai ta

--- Conexion.cs
+++ Conexion.cs
@@ -11,7 +11,7 @@ namespace IntegradoraPOO
 
         public static MySqlConnection conexion()
         {
-            string servidor = "127.0.0.1";
+            string servidor = "10.1.124.255";
             string bd = "chismes";
             string user = "vivianos";
             string password = "vivianos123";
@@ -29,8 +29,8 @@ namespace IntegradoraPOO
                 Console.WriteLine(ex);
                 return null;
             }
-         
+
         }
     }
-    
+
 }
